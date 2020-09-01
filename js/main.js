36c3f9aafe2c9eee5a609edfbba05f8b61c83bda
T-Ralph <olaniyitralph@gmail.com>
2020-09-01 15:52:13 -0600

    Added JS to process Contact Form;

--- js/main.js
+++ js/main.js
@@ -18,6 +18,37 @@ menuToggle = () => {
         menuMobileDisplay = true; //Set menuMobileDisplay
     }
 }
+submitContactForm = () => {
+    event.preventDefault(); //Prevent Form Refresh
+
+    //Get Values
+    const contactForm = document.querySelector("section#contact > div#form > form");
+    const name = document.querySelector("section#contact > div#form > form > input[name=name]").value;
+    const email = document.querySelector("section#contact > div#form > form > input[name=email]").value;
+    const subject = document.querySelector("section#contact > div#form > form > input[name=subject]").value;
+    const message = document.querySelector("section#contact > div#form > form > textarea[name=message]").value;
+
+    //Array of Rejected Swear Words
+    const swearWords = ["feldercarb", "frack", "skinjob", "vulgacarb"];
+    let containsSwearWords = false;
+
+    //Check for swearWords
+    for (const swearWord of swearWords) {
+        if (subject.includes(swearWord) || message.includes(swearWord)) {
+            containsSwearWords = true;
+        }
+    }
+
+    //Check if swearWords is present
+    if (containsSwearWords == false) {
+        window.location.href = encodeURI(`mailto:Tosin Raphael Olaniyi<hello@tosinraphaelolaniyi.com>?subject=${subject}&body=${message}`);
+    }
+    else {
+        alert("Let's be professional! No swear words can be used.");
+    }
+
+    contactForm.reset();//Reset Form
+}
 
 //Add Event Listener to Menu Icon
 const menuIcon = document.querySelector("header > nav > i");
@@ -26,3 +57,7 @@ menuIcon.addEventListener("click", menuToggle);
 //Add Event Listener to Navigation List
 const navigationList = document.querySelector("header > nav > ul");
 navigationList.addEventListener("click", menuToggle);
+
+//Add Event Listener to Form
+const contactForm = document.querySelector("section#contact > div#form > form");
+contactForm.addEventListener("submit", submitContactForm);
