ab6b49768a72f8a5eaf9e0b692fad92cf5dcb79c
T-Ralph <olaniyitralph@gmail.com>
2020-09-03 06:48:19 -0600

    Added Portfolio Filter Functionality

--- js/main.js
+++ js/main.js
@@ -18,6 +18,38 @@ menuToggle = () => {
         menuMobileDisplay = true; //Set menuMobileDisplay
     }
 }
+filterPortfolios = (portfolioClass) => {
+    //Get Portfolios Filter Buttons
+    const portfolioFilterButtons = document.querySelectorAll("main > article > section#portfolio > div.portfolios-filters > button.filter-button");
+
+    //Get Portfolios Containers
+    const portfolioContainers = document.querySelectorAll("main > article > section#portfolio > div.portfolios > div.portfolio-container");
+
+    //Remove 'active' Class From All Buttons, Then, Add 'active' Class To The Clicked Button
+    for (const portfolioFilterButton of portfolioFilterButtons) {
+        if (portfolioFilterButton.classList.contains("active")) {
+            portfolioFilterButton.classList.remove("active");
+        }
+        if (portfolioFilterButton.classList.contains(`filter-${portfolioClass}`)) {
+            portfolioFilterButton.classList.add("active");
+        }
+    }
+
+    //Add 'hide' Class To All Portfolios Containers and Add 'show' Class To The Filtered Portfolio
+    for (const portfolioContainer of portfolioContainers) {
+        if (portfolioContainer.classList.contains("show")) {
+            portfolioContainer.classList.remove("show");
+        }
+        if (portfolioContainer.classList.contains("hide")) {
+            portfolioContainer.classList.remove("hide");
+        }
+        portfolioContainer.classList.add("hide");
+        if (portfolioContainer.classList.contains(portfolioClass)) {
+            portfolioContainer.classList.remove("hide");
+            portfolioContainer.classList.add("show");
+        }
+    }
+}
 submitContactForm = () => {
     event.preventDefault(); //Prevent Form Refresh
 
@@ -57,6 +89,14 @@ menuIcon.addEventListener("click", menuToggle);
 //Add Event Listener to Navigation List
 const navigationList = document.querySelector("header > nav > ul");
 navigationList.addEventListener("click", menuToggle);
+
+//Add Event Listener to Portfolio Filter Buttons
+const portfolioFilterButtons = document.querySelectorAll("main > article > section#portfolio > div.portfolios-filters > button.filter-button");
+portfolioFilterButtons[0].addEventListener("click", function(){filterPortfolios(portfolioClass = "all")}, false);
+portfolioFilterButtons[1].addEventListener("click", function(){filterPortfolios(portfolioClass = "websites")}, false);
+portfolioFilterButtons[2].addEventListener("click", function(){filterPortfolios(portfolioClass = "windows")}, false);
+portfolioFilterButtons[3].addEventListener("click", function(){filterPortfolios(portfolioClass = "android")}, false);
+portfolioFilterButtons[4].addEventListener("click", function(){filterPortfolios(portfolioClass = "github")}, false);
 
 //Add Event Listener to Form
 const contactForm = document.querySelector("section#contact > div#form > form");
